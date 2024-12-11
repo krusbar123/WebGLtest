95b6439b706d1f25e47cfe4214e968dbb71e400b
krusbar123 <leifbooom@gmail.com>
2024-12-11 18:26:34 +0100

    Update WebGLtest.js

--- WebGLtest.js
+++ WebGLtest.js
@@ -4,19 +4,29 @@ main();
 
 function main() {
     const canvas = document.getElementById("glcanvas");
-  // Initialize the GL context
-  const gl = canvas.getContext("webgl");
+    // Initialize the GL context
+    const gl = canvas.getContext("webgl");
 
-  // Only continue if WebGL is available and working
-  if (!gl) {
+    // Only continue if WebGL is available and working
+    if (!gl) {
     alert(
-      "Unable to initialize WebGL. Your browser or machine may not support it.",
+    "Unable to initialize WebGL. Your browser or machine may not support it.",
     );
     return;
   }
 
-  // Set clear color to black, fully opaque
-  gl.clearColor(0.0, 0.0, 1.0, 1.0);
-  // Clear the color buffer with specified clear color
-  gl.clear(gl.COLOR_BUFFER_BIT);
+var vertices = [
+    -1.0, 1.0, 0.0,
+    -1.0, -1.0, 0.0,
+    1.0, -1.0, 0.0
+];
+
+const vShader = '
+    
+
+    
+// Set clear color to black, fully opaque
+gl.clearColor(0.0, 0.0, 1.0, 1.0);
+// Clear the color buffer with specified clear color
+gl.clear(gl.COLOR_BUFFER_BIT);
 }
