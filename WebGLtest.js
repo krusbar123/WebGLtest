3673109926455c2240ef9c10298d7a96c8c7168e
krusbar123 <leifbooom@gmail.com>
2024-12-12 13:01:32 +0100

    Update WebGLtest.js

--- WebGLtest.js
+++ WebGLtest.js
@@ -63,7 +63,7 @@ gl.compileShader(vertShader);
 var fragCode = 
     'uniform sampler2D ground;' +
     'void main(void) {' +
-    'gl_FragColor = vec4(0.7, 0.5, 1.0, 1.0);' +
+    'gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);' +
     '}';
 
 var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
