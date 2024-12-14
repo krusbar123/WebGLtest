909ff6c716474628b25fcb6e74c7d90f9590b312
krusbar123 <leifbooom@gmail.com>
2024-12-14 17:27:52 +0100

    Update WebGLtest.js

--- WebGLtest.js
+++ WebGLtest.js
@@ -77,8 +77,12 @@ function main() {
         'vec2 current = gl_FragCoord.xy / screen;' +
         'vec2 over = current + vec2(0.0, 1.0 / screen.y);' +
         'vec2 under = current - vec2(0.0, 1.0 / screen.y);' +
-        'if (!(texture2D(ground, under).x) && !(texture2D(ground, under).y) && !(texture2D(ground, under).z)) {' +
+        'if (texture2D(ground, under).x == 0.0 && texture2D(ground, under).y == 0.0 && texture2D(ground, under).z == 0.0) {' +
+        'if (over.y > (screen.y / 2.0)) {' +
+        'gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);' +
+        '} else {' +
         'gl_FragColor = texture2D(ground, over);' +
+        '}' +
         '} else {' +
         'gl_FragColor = texture2D(ground, current);' +
         '}' +
