a57ff514c43ed46029fc6e73ca5816eeb105b1cc
krusbar123 <leifbooom@gmail.com>
2024-12-15 22:01:43 +0100

    Update WebGLtest.js

--- WebGLtest.js
+++ WebGLtest.js
@@ -191,9 +191,7 @@ function main() {
         
     // Set clear color to black, fully opaque
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
-    
-    gl.enable(gl.DEPTH_TEST);
-        
+
     // Clear the color buffer with specified clear color
     gl.clear(gl.COLOR_BUFFER_BIT);
     
